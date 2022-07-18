8bed120080133e818a231476560999520c14ca0e
a.agataev <a.agataev@corp.mail.ru>
2022-07-18 10:25:57 +0300

    Initial Commit

---
+++ netology-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  netology-swiftui
+//
+//  Created by a.agataev on 18.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ netology-swiftui/netology_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  netology_swiftuiApp.swift
+//  netology-swiftui
+//
+//  Created by a.agataev on 18.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct netology_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
